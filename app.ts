1f0606d110fb5ba5a86fdcc2579fc4ec88a11a72
Marvyn686 <andrey.rybalko.93@mail.ru>
2025-02-20 22:23:20 +0300

    save100

--- app.ts
+++ app.ts
@@ -1,42 +1,68 @@
-const list:HTMLElement | null = document.querySelector('#list')
-const filter:HTMLElement | null = document.querySelector('#filter')
-let USERS:any[] = []
+// 1 способ получения ссылок элементов из HTML
+const list = document.getElementById('list') as HTMLElement;
+// 2 способ получения ссылок элементов из HTML
+const filter = document.querySelector('#filter') as HTMLElement | null;
+//проверка на получение null, если null то выдавать сообщение
+if (!list || !filter) {
+    throw new Error('Элемент отсутствует')
+}
+
+interface User{
+
+    id: number;
+    name: string;
+
+}
+
+let USERS:User[] = []
 
 filter.addEventListener('input', (event:Event):void => {
-    // @ts-ignore
-    const value:any = event.target.value.toLowerCase()
-    const filteredUsers:any[] = USERS.filter((user:any):any =>
+    //вывод отдельно момента, когда пользователь выбирает место, где писать текст + плюс точно говорю, что данное действие есть
+    const target = event.target as HTMLInputElement
+    // получение значения из строки поиска в нижнем регистре
+    const value:string = target.value.toLowerCase()
+    //
+    const filteredUsers:User[] = USERS.filter((user:User):boolean =>
         user.name.toLowerCase().includes(value)
     )
     render(filteredUsers)
 })
 
-// @ts-ignore
+
 async function start(): Promise<void> {
     list.innerHTML = 'Loading...'
     try {
-        const resp:Response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const data:any = await resp.json()
+        const resp: Response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data:User[] = await resp.json()
         setTimeout(():void => {
             USERS = data
             render(data)
         }, 2000)
-    } catch (err) {
-        list.style.color = 'red'
-        list.innerHTML = err.message
+        //проверка на получения ошибки
+        // тип у err: unknown - по умолчанию.
+    } catch (err: unknown) {
+        list.style.color = 'red';
+        //если ошибка , то выдавать сообщение
+        if (err instanceof Error) {
+            list.innerHTML = err.message
+        }
+
     }
 }
 
-function render(users:any[] = []):void {
+function render(users:User[] = []):void {
+    //проверка на пустой массив, если пуст то выдавать сообщение
     if (users.length === 0) {
         list.innerHTML = 'Ничего не найдено!'
     } else {
-        const html:string = users.map(toHTML).join('')
-        list.innerHTML = html
+        //отображение списка пользователь
+        //html подсвечивалось желтым, избыточная константа, которая использовалась один раз.
+        //const html:string = users.map(toHTML).join('')
+        list.innerHTML = users.map(toHTML).join('')
     }
 }
 
-function toHTML(user:any):string {
+function toHTML(user:User):string {
     return `
     <li class="list-group-item">${user.name}</li>
   `
